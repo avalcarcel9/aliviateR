ab0bd864ed8e49aa18db99ffb0ba50f2db2e6790
avalcarcel9 <alval@pennmedicine.upenn.ed>
2019-06-10 13:45:16 -0400

    updating load_mrdata to allow for .rds files

--- R/load_mrdata.R
+++ R/load_mrdata.R
@@ -2,10 +2,30 @@
 #' @description Loads multiple .RData and .rda files
 #' @param path The path to the folder where the multiple files are stored
 #' @param pattern The pattern of the files inside the folder to import
+#' @importFrom stringr str_detect
+#' @importFrom dplyr bind_rows
 #' @export
 
-load_mrdata <- function(path, pattern){
+load_mrdata <- function(path, pattern, bind_rows = TRUE){
+  # Load files from given path
   files = list.files(path = path, pattern = pattern, full.names = TRUE)
-  data = sapply(files, function(x) mget(load(x)), simplify = TRUE)
+  # Are files all .RData
+  if(all(stringr::str_detect(files, '.RData')) == TRUE){
+    data = sapply(files, function(x) mget(load(x)), simplify = TRUE)
+  }
+  # Are files all .rds
+  else if(all(stringr::str_detect(files, '.rds')) == TRUE){
+    data = sapply(files, function(x) mget(readRDS(x)), simplify = TRUE)
+  }
+  # If not .RData or .rds stop the function
+  else if(all(stringr::str_detect(files, '.RData')) == FALSE | all(stringr::str_detect(files, '.rds')) == FALSE){
+    stop('Must provide .RData or .rds files.')
+  }
+
+  # If bind_rows option is true bind the rows of the data
+  if(bind_rows == TRUE){
+    data = dplyr::bind_rows(data, .id = "file")
+  }
+
   return(data)
 }
